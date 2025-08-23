cfeb2be7a81c9a6d322333c6bb306b2a4ab25b62
Aditya Tile <adityatile99@gmail.com>
2025-08-23 14:32:28 +0530

    add sidebar

---
+++ frontend/src/components/ChatWindow.jsx
@@ -0,0 +1,32 @@
+import "../css/ChatWindow.css";
+import Chat from "./Chat";
+
+const ChatWindow = () => {
+  return (
+    <div className="chatWindow">
+      <div className="navbar">
+        <span>
+          NeoGPT <i class="fa-solid fa-chevron-down"></i>
+        </span>
+        <div className="userIconDiv">
+          <span>
+            <i class="fa-solid fa-user"></i>
+          </span>
+        </div>
+      </div>
+      <Chat></Chat>
+      <div className="chatInput">
+        <div className="userInput">
+          <input type="text" placeholder="Ask anything"></input>
+          <i class="fa-solid fa-paper-plane"></i>
+        </div>
+        <p className="info">
+          NeoGPT can make mistakes. Check important info. See Cookie
+          Preferences.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default ChatWindow;
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import "../css/Sidebar.css";
+
+const Sidebar = () => {
+  return (
+    <section className="sidebar">
+      <button>
+        <img src="src/assets/blacklogo.png" alt="bg" className="logo"></img>
+        <i className="fa-solid fa-pen-to-square"></i>
+      </button>
+    </section>
+  );
+};
+
+export default Sidebar;
