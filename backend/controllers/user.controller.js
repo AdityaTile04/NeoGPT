87dbae0d7706bb156538c55cd8a9831fb714a04d
Aditya Tile <adityatile99@gmail.com>
2025-10-11 23:25:03 +0530

    feat(auth): add login route and controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,5 +1,8 @@
 import { User } from "../models/User.js";
 import bcrypt from "bcryptjs";
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+dotenv.config();
 
 export const signup = async (req, res) => {
   const { username, email, password } = req.body;
@@ -17,4 +20,29 @@ export const signup = async (req, res) => {
   await user.save();
 
   res.status(200).json({ message: "User created successfully" });
+};
+
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      res.status(500).json({ message: "Invalid crendintials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      res.status(500).json({ message: "Invalid password" });
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.SECRET, {
+      expiresIn: "1h",
+    });
+
+    res.status(200).json({ token, message: "Login Successfull" });
+  } catch (err) {
+    console.log(err);
+  }
 };
